c5bdc804af909be6118377d92ea482e49fff2f06
dcicc4 <dcicc4@github.com>
2018-08-25 14:33:53 -0400

    Added AIPlayer

---
+++ src/Players/AIPlayer.java
@@ -0,0 +1,21 @@
+package Players;
+
+import Utilities.Move;
+import Utilities.StateTree;
+
+public class AIPlayer extends Player {
+
+	public AIPlayer(String n, int t, int l) {
+		super(n, t, l);
+	}
+
+	/* (non-Javadoc)
+	 * @see Players.Player#getMove(Utilities.StateTree)
+	 */
+	@Override
+	public Move getMove(StateTree state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
