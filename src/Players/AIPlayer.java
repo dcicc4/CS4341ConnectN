c9a4a17777ec9f3d99f40d329417d53c9114d08f
dcicc4 <dcicc4@github.com>
2018-08-29 12:12:50 -0400

    Fixed max min

--- src/Players/AIPlayer.java
+++ src/Players/AIPlayer.java
@@ -30,8 +30,10 @@ public class AIPlayer extends Player {
 			if (this.containsWinForPlayerNumber(this.turn, state))
 				return 1;
 		} else {
-			if (this.containsWinForPlayerNumber(Math.abs(3-this.turn), state)) // containsDefeat checks if the state has connectedNeeded number of tiles in row
-										// for opponent
+			if (this.containsWinForPlayerNumber(Math.abs(3 - this.turn), state)) // containsDefeat checks if the state
+																					// has connectedNeeded number of
+																					// tiles in row
+				// for opponent
 				return -1;
 		}
 		int value = 0;
@@ -39,30 +41,20 @@ public class AIPlayer extends Player {
 		LinkedList<Integer> values = new LinkedList<Integer>();
 
 		for (OurStateTree aState : state.getStatesAfterValidMoves()) {
-
-			int currentValue = getValueOfState(aState, min, max); // TODO: Do we have to reset min and max at some
-																	// point?
-
-			values.push(currentValue);
-
 			if (state.isMyTurn) {
-
+				int currentValue = getValueOfState(aState, null, max);
+				values.push(currentValue);
 				if (min != null && value >= min) {
-
 					return currentValue;
 				}
-
 				max = Collections.max(values);
-
 			} else {
-
+				int currentValue = getValueOfState(aState, min, null);
+				values.push(currentValue);
 				if (max != null && value <= max)
-
 					return currentValue;
-
 				min = Collections.min(values);
 			}
-
 			return state.isMyTurn ? max : min;
 		}
 		return value;
@@ -70,56 +62,56 @@ public class AIPlayer extends Player {
 
 	private boolean containsWinForPlayerNumber(int playerNumber, OurStateTree state) {
 		if (state.lastMove.getPop()) {
-			//TODO implement pop check
-		}else {
-			//TODO check diagnols 
-			return checkVertical(playerNumber, state) || checkLeftHorizontal(playerNumber, state) || checkRightHorizontal(playerNumber,state);
+			// TODO implement pop check
+		} else {
+			// TODO check diagnols
+			return checkVertical(playerNumber, state) || checkLeftHorizontal(playerNumber, state)
+					|| checkRightHorizontal(playerNumber, state);
 		}
 
 		return false;
 	}
-private boolean checkLeftHorizontal(int playerNumber, OurStateTree state) {
-	int column = state.lastMove.getColumn();
-	int row = state.getRowForLastMove();
-	if (column < state.winNumber - 1 ) {
-		return false;
-	}
-	for (int i = column; i>(column - state.winNumber); i--) {
-		if(state.getBoardMatrix()[row][i]!= playerNumber) {
-			return false;
-		}
-	}
-	return true;
-	}
 
-private boolean checkRightHorizontal(int playerNumber, OurStateTree state) {
-	int column = state.lastMove.getColumn();
-	int row = state.getRowForLastMove();
-	if (state.columns - column < state.winNumber) {
-		return false;
-	}
-	for (int i = column; i<(column + state.winNumber); i++) {
-		if(state.getBoardMatrix()[row][i]!= playerNumber) {
-			return false;
-		}
-	}
-	return true;
-	}
-
-
-	private boolean checkVertical(int playerNumber, OurStateTree state) {
+	private boolean checkLeftHorizontal(int playerNumber, OurStateTree state) {
 		int column = state.lastMove.getColumn();
 		int row = state.getRowForLastMove();
-		if (row < state.winNumber - 1 ) {
+		if (column < state.winNumber - 1) {
 			return false;
 		}
-		for (int i = row; i>(row - state.winNumber); i--) {
-			if(state.getBoardMatrix()[i][column]!= playerNumber) {
+		for (int i = column; i > (column - state.winNumber); i--) {
+			if (state.getBoardMatrix()[row][i] != playerNumber) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
+	private boolean checkRightHorizontal(int playerNumber, OurStateTree state) {
+		int column = state.lastMove.getColumn();
+		int row = state.getRowForLastMove();
+		if (state.columns - column < state.winNumber) {
+			return false;
+		}
+		for (int i = column; i < (column + state.winNumber); i++) {
+			if (state.getBoardMatrix()[row][i] != playerNumber) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private boolean checkVertical(int playerNumber, OurStateTree state) {
+		int column = state.lastMove.getColumn();
+		int row = state.getRowForLastMove();
+		if (row < state.winNumber - 1) {
+			return false;
+		}
+		for (int i = row; i > (row - state.winNumber); i--) {
+			if (state.getBoardMatrix()[i][column] != playerNumber) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
